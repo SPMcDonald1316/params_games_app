1aefb5850fdc93bad2bd594e5b5b159a3599fc11
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-04 18:34:13 -0600

    add route, render file, and add logic to display query name and message

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,2 +1,10 @@
 class Api::GamesController < ApplicationController
+  def query
+    @user_name = params["name"]
+    @user_name = @user_name.upcase
+    if @user_name.chars.first == "A"
+      @msg = "Hey, your name starts with the first letter of the alphabet!"
+    end
+    render 'query.json.jb'
+  end
 end
